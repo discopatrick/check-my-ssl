b75ea95caa1cf74c9742ddf8d21736f84cc0d4c4
Patrick Heneghan <discopatrick@gmail.com>
2018-10-15 16:51:14 +0100

    Create URLForm component and render in App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import URLForm from './URLForm.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Enter a URL to check its SSL.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <URLForm />
         </header>
       </div>
     );
