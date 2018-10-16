f886ebcca52fa932e0540d9d44795be9c76da4d9
Patrick Heneghan <discopatrick@gmail.com>
2018-10-16 11:07:47 +0100

    Allow CORS in backend

--- backend/__init__.py
+++ backend/__init__.py
@@ -4,10 +4,12 @@ from flask import (
     Flask,
     jsonify,
 )
+from flask_cors import CORS
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     @app.route('/hello')
     def hello():
