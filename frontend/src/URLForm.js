996180493a3efc3d73328b4cf16f16972d90a17d
Patrick Heneghan <discopatrick@gmail.com>
2018-10-16 11:10:12 +0100

    Consume API from frontend

--- frontend/src/URLForm.js
+++ frontend/src/URLForm.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 class URLForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {
+      actionNeeded: null,
+      daysUntilSSLExpiry: null,
+      value: '',
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -14,19 +18,32 @@ class URLForm extends Component {
   }
 
   handleSubmit(event) {
-    alert('A URL was submitted: ' + this.state.value);
+    fetch("http://localhost:5000/check-ssl")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+          this.setState({
+            actionNeeded: result.action_needed,
+            daysUntilSSLExpiry: result.days_until_ssl_expiry,
+          })
+        }
+      )
     event.preventDefault();
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          URL:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            URL:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <p>Days until SSL expiry: {this.state.daysUntilSSLExpiry}</p>
+      </div>
     )
   }
 }
