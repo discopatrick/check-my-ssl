3f2604f84fd1dedd383ae7104ee103bd12bcef03
Patrick Heneghan <discopatrick@gmail.com>
2018-10-16 09:51:27 +0100

    Add skeleton Flask app

---
+++ backend/__init__.py
@@ -0,0 +1,13 @@
+import os
+
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello World!'
+
+    return app
