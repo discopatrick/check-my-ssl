b7ecd7dfac078584d4e52d022b10abdaad043758
Patrick Heneghan <discopatrick@gmail.com>
2018-10-19 14:00:47 +0100

    Convert print statements to logs

--- backend/jobs.py
+++ backend/jobs.py
@@ -1,3 +1,4 @@
+import logging
 from socket import gaierror
 
 import dramatiq
@@ -16,10 +17,10 @@ def days_until_ssl_expiry_job(hostname):
     try:
         days_left = days_until_ssl_expiry(hostname)
     except gaierror as e:
-        print(f'Error processing job: {str(e)}')
+        logging.error(f'Error processing job: {str(e)}')
         return
 
-    print(f'{hostname} SSL has {days_left} days until expiry')
+    logging.info(f'{hostname} SSL has {days_left} days until expiry')
     domain_name = get_or_create(session=db_session, model=DomainName, domain_name=hostname)
     db_session.add(SSLCheck(domain_name=domain_name, days_until_ssl_expiry=days_left))
     db_session.commit()
