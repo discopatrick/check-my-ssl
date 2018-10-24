0e7b0acbbc53ab8299f0e8b3cb05e1e134ec7a85
Patrick Heneghan <discopatrick@gmail.com>
2018-10-24 15:52:20 +0100

    Add component, link, and route for DomainNames list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import './App.css';
+import { DomainNames } from './DomainNames.js';
 import URLForm from './URLForm.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Enter a URL to check its SSL.
-          </p>
-          <URLForm />
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <p>
+              Enter a URL to check its SSL.
+            </p>
+            <URLForm />
+            <Link to='/domain-names'>
+              View all domain names
+            </Link>
+            <Route exact path='/domain-names' component={DomainNames} />
+          </header>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ frontend/src/DomainNames.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class DomainNames extends Component {
+  render() {
+    return (
+      <p>List domain names here.</p>
+    )
+  }
+}
+
+export { DomainNames };
