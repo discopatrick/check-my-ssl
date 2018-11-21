0611bed0aea804abc0d45ea8329e98313276c3d5
discopatrick <discopatrick@gmail.com>
2018-11-21 16:30:48 +0000

    Improve logging in web.jobs

--- backend/web/web/jobs.py
+++ backend/web/web/jobs.py
@@ -11,9 +11,12 @@ from ssl_checker import days_until_ssl_expiry
 rabbitmq_broker = RabbitmqBroker(host='broker')
 dramatiq.set_broker(rabbitmq_broker)
 
+LOG = logging.getLogger(__name__)
+
 
 @dramatiq.actor
 def days_until_ssl_expiry_job(hostname):
+    LOG.info('days_until_ssl_expiry_job')
     try:
         days_left = days_until_ssl_expiry(hostname)
     except gaierror as e:
@@ -22,7 +25,7 @@ def days_until_ssl_expiry_job(hostname):
 
     # funky = 1 / 0  # TODO: ensure this gets to Sentry (import Sentry config at the top of this file?)
 
-    logging.info(f'{hostname} SSL has {days_left} days until expiry')
+    LOG.info(f'{hostname} SSL has {days_left} days until expiry')
     domain_name = get_or_create(session=db_session, model=DomainName, domain_name=hostname)
     db_session.add(SSLCheck(domain_name=domain_name, days_until_ssl_expiry=days_left))
     db_session.commit()
@@ -30,6 +33,7 @@ def days_until_ssl_expiry_job(hostname):
 
 @dramatiq.actor
 def check_ssl_for_all_domain_names():
+    LOG.info('check_ssl_for_all_domain_names')
     all_domain_names = db_session.query(DomainName).all()
     for dn in all_domain_names:
         days_until_ssl_expiry_job.send(dn.domain_name)
